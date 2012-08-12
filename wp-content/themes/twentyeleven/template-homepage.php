5bcf85e0e4bc7b8d9c55ac8433c7bdb9c2d4ea91
Tyler Graf <tylergraf@gmail.com>
2012-08-12 12:37:20 -0600

    adding slideshow on homepage.

--- wp-content/themes/twentyeleven/template-homepage.php
+++ wp-content/themes/twentyeleven/template-homepage.php
@@ -17,5 +17,34 @@
 get_header(); ?>
 <div id="bd" class="wrapper">
     <?php the_post(); echo get_the_content(); ?>
+<!--  <div id="slides">-->
+<!--				<div class="slides_container">-->
+<!--					<a href="http://www.flickr.com/photos/jliba/4665625073/" title="145.365 - Happy Bokeh Thursday! | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-1.jpg" width="570" height="270" alt="Slide 1"></a>-->
+<!--					<a href="http://www.flickr.com/photos/stephangeyer/3020487807/" title="Taxi | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-2.jpg" width="570" height="270" alt="Slide 2"></a>-->
+<!--					<a href="http://www.flickr.com/photos/childofwar/2984345060/" title="Happy Bokeh raining Day | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-3.jpg" width="570" height="270" alt="Slide 3"></a>-->
+<!--					<a href="http://www.flickr.com/photos/b-tal/117037943/" title="We Eat Light | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-4.jpg" width="570" height="270" alt="Slide 4"></a>-->
+<!--					<a href="http://www.flickr.com/photos/bu7amd/3447416780/" title="“I must go down to the sea again, to the lonely sea and the sky; and all I ask is a tall ship and a star to steer her by.” | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-5.jpg" width="570" height="270" alt="Slide 5"></a>-->
+<!--					<a href="http://www.flickr.com/photos/streetpreacher/2078765853/" title="twelve.inch | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-6.jpg" width="570" height="270" alt="Slide 6"></a>-->
+<!--					<a href="http://www.flickr.com/photos/aftab/3152515428/" title="Save my love for loneliness | Flickr - Photo Sharing!" target="_blank"><img src="http://slidesjs.com/examples/standard/img/slide-7.jpg" width="570" height="270" alt="Slide 7"></a>-->
+<!--				</div>-->
+<!--				<a href="#" class="prev"><img src="img/arrow-prev.png" width="24" height="43" alt="Arrow Prev"></a>-->
+<!--				<a href="#" class="next"><img src="img/arrow-next.png" width="24" height="43" alt="Arrow Next"></a>-->
+<!--			</div>-->
 </div>
+
+
 <?php get_footer(); ?>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://gsgd.co.uk/sandbox/jquery/easing/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="/wp-content/themes/twentyeleven/js/slides.min.jquery.js"></script>
+	<script type="text/javascript">
+      $('#slides').slides({
+        preload: true,
+        preloadImage: '/wp-content/themes/twentyeleven/js/img/loading.gif',
+        play: 5000,
+        pause: 2500,
+        hoverPause: true
+//        crossfade: true,
+//        fadeSpeed: 350
+      });
+  </script>
